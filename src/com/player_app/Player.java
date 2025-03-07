ed7364bb34fb2f4f87fa2fea9d62102c9dfd424f
Giorgi Giguashvili <giguashviligiorgi50@gmail.com>
2025-03-07 22:13:06 +0400

    created basic project architecture and player class

---
+++ src/com/player_app/Player.java
@@ -0,0 +1,21 @@
+package com.player_app;
+
+import java.util.ArrayList;
+
+// The Player class holds the player's stats and inventory.
+class Player {
+    int health;
+    ArrayList<String> inventory;
+    private final int MaxHealth = 100;
+
+    public Player() {
+        this.health = MaxHealth;
+        this.inventory = new ArrayList<>();
+    }
+
+    // A method to display the player's current stats.
+    public void showStats() {
+        System.out.println("Player Health: " + health);
+        System.out.println("Inventory: " + inventory);
+    }
+}
